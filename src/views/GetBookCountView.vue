a4d0f458054629f9bc20a2ea0ae38a29b4a966ed
Lachlan Williams <lwil0039@student.monash.edu>
2024-09-24 15:17:49 +1000

    Week 9 activity 9.4.1 complete

--- src/views/GetBookCountView.vue
+++ src/views/GetBookCountView.vue
@@ -20,7 +20,7 @@ export default {
     methods: {
         async getBookCount(){
             try {
-                const response = await axios.get('');
+                const response = await axios.get('https://countbooks-7rz7hwrebq-uc.a.run.app');
                 this.count = response.data.count;
                 this.error = null;
             } catch (error) {
